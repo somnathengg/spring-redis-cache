972b87833e0f36578877caba2e7f911b3ae38ecd
somnathengg <somnath8383@gmail.com>
2025-06-01 11:00:42 +0530

    changes

--- src/main/java/com/spring/redis/controller/ItemController.java
+++ src/main/java/com/spring/redis/controller/ItemController.java
@@ -35,7 +35,6 @@ public class ItemController {
 
     @PostMapping("/create-item")
     public Item createItem(@RequestBody Item request) {
-    	System.out.println("inside create item");
         return itemService.createItem(request);
     }
 
